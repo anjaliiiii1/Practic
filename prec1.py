c68d264a482b49c971fffe61705d91414c77f27a
CodeWithAnjali <ay4567430@example.com>
2025-06-02 16:43:30 +0530

    Create game find biggest number using Condintion

--- prec1.py
+++ prec1.py
@@ -1,1 +1,12 @@
-print("Anjali Yadav")
+a = int(input("Enter your 1st Number : "))
+b = int(input("Enter your 2nd Number : "))
+c = int(input("Enter your 3rd Number : "))
+
+if a>b | a>c:
+    print("the biggest number is : ", a)
+
+elif b>a | b>c:
+    print("the biggest number is : ", b)
+
+else:
+    print("the biggest number is : ", c)
