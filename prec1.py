958652b76ff57f609cffe1cfdb64cce1d63a386e
CodeWithAnjali <ay4567430@example.com>
2025-06-03 17:59:36 +0530

    add limit try in guessing  game

--- prec1.py
+++ prec1.py
@@ -1,16 +1,23 @@
 import random
 
 Secret_Num = random.randint(1,20)
-user = int(input("Guess the secret number : "))
 
-while user!= Secret_Num:
+attempts = 0
 
-    if user > Secret_Num:
+un_attempts = 3
+
+while attempts < un_attempts:
+
+    user = int(input("Guess the secret number : "))
+    attempts +=1
+    if Secret_Num == user:
+        print(f"Congratulations! you won at {attempts}tris.")
+        break;
+    
+    elif user > Secret_Num:
         print("Too High")
     else:
         print ("too low")
 
-    user = int(input("try again : "))
-
-print("Congratulations!")
+print("Game Over The correct number is : ", Secret_Num)
     
