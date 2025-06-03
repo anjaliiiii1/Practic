078fff02867d6fe388bda9040379aad3b9804f2c
CodeWithAnjali <ay4567430@example.com>
2025-06-03 10:48:27 +0530

    Reverse number

--- prec1.py
+++ prec1.py
@@ -1,12 +1,5 @@
-a = int(input("Enter your 1st Number : "))
-b = int(input("Enter your 2nd Number : "))
-c = int(input("Enter your 3rd Number : "))
+num = 10
 
-if a>b | a>c:
-    print("the biggest number is : ", a)
-
-elif b>a | b>c:
-    print("the biggest number is : ", b)
-
-else:
-    print("the biggest number is : ", c)
+while num >= 1:
+    print(num)
+    num -= 1
