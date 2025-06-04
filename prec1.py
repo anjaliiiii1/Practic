ee89a9e03d1528162a18c4fc5e233d9bc3ab267e
CodeWithAnjali <ay4567430@example.com>
2025-06-04 17:57:59 +0530

    add time lib and user gusse num Store

--- prec1.py
+++ prec1.py
@@ -1,14 +1,17 @@
 import random
-
+import time
 Secret_Num = random.randint(1,20)
 
 attempts = 0
 
 un_attempts = 3
 
+gusses = []
+
 while attempts < un_attempts:
 
     user = int(input("Guess the secret number : "))
+    gusses.append(user)
     attempts +=1
     if Secret_Num == user:
         print(f"Congratulations! you won at {attempts}tris.")
@@ -16,8 +19,11 @@ while attempts < un_attempts:
     
     elif user > Secret_Num:
         print("Too High")
+        time.sleep(0.05)
+
     else:
         print ("too low")
-
+print("Your guesses number were",user)
 print("Game Over The correct number is : ", Secret_Num)
+
     
